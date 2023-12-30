18004f5142bb66bd3a416691429d58c9b97c99c9
svitlana-horodylova <horodylova.sv@gmail.com>
2023-12-30 15:56:02 +0000

    Updated request

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import { config } from './config';
 const { apiKey, baseUrl } = config;
 
 export const getTrendingMovies = async () => {
-  const url = `${baseUrl}/trending/movie/day?api_key=${apiKey}`;
+  const url = `${baseUrl}trending/movie/week?api_key=${apiKey}`;
   
   try {
     const response = await axios.get(url);
--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 export const config = {
     apiKey: 'a3419bb2c415f83d30bbc28c2bb776df',
-    baseUrl: 'https://www.themoviedb.org/',
+    baseUrl: 'https://api.themoviedb.org/3',
   };
   
   
